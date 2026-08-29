70a5105a58261fcde8fa43c086a33eeaadf2b70f
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-29 11:34:28 +0600

    using with function

--- temLiteral.js
+++ temLiteral.js
@@ -3,3 +3,11 @@ let price = 30;
 let quantity = 3;
 
 console.log(`Total Price: ${price * quantity} BDT`);
+
+// using Function
+function getDiscount(price,percentage){
+    let discount =  (price*percentage)/100;
+    return price - discount;
+}
+
+console.log(`Discount Price: ${getDiscount(1200,20)}`);
