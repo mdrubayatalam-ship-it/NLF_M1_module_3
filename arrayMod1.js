e54b11a19de796f12abe6a93c4b7c3b511fc9137
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-30 22:44:44 +0600

    New Up

--- arrayMod1.js
+++ arrayMod1.js
@@ -1,7 +1,14 @@
-
+// forEach,map,filter
 
 let fruits = ["Apple","Banana","Litchi"];
 
 let newFruits = fruits.forEach((f,idx)=>{
     console.log(`${idx + 1} -->${f}`);
+    // return `${idx + 1} --> ${f}` ;  //Return korle kintu kichu asbena
 });
+
+let newFruits2 = fruits.map((f,idx)=>{
+    return `${idx + 1} -->${f}`;
+})
+
+console.log(newFruits);
