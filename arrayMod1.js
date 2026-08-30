016c7b661e59068baa83ce744e1e3f660a1e8301
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-30 22:54:55 +0600

    forEach, map and filter has been included

--- arrayMod1.js
+++ arrayMod1.js
@@ -1,14 +1,19 @@
 // forEach,map,filter
 
-let fruits = ["Apple","Banana","Litchi"];
+let fruits = ["Apple","Banana","Litchi","Berry","Pine Apple"];
 
-let newFruits = fruits.forEach((f,idx)=>{
-    console.log(`${idx + 1} -->${f}`);
-    // return `${idx + 1} --> ${f}` ;  //Return korle kintu kichu asbena
-});
+// let newFruits = fruits.forEach((f,idx)=>{
+//     console.log(`${idx + 1} -->${f}`);
+//     // return `${idx + 1} --> ${f}` ;  //forEach New array return korbena
+// });
 
-let newFruits2 = fruits.map((f,idx)=>{
-    return `${idx + 1} -->${f}`;
-})
+// let newFruits2 = fruits.map((f,idx)=>{
+    // return `${idx + 1} -->${f}`; // map new array return kore
+// })
 
-console.log(newFruits);
+// console.log(newFruits);
+
+
+// filter
+let newFruits2 = fruits.filter((f)=>f.length>4); //fruits er name gula toh string tai na..so ei name gular toh length ase..ei wise ami jamne chai oi condition dibo
+console.log(newFruits2);
