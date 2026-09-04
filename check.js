934a967b484774a2cade03ebc8cf4e8b7393a10f
bitbyrubayat <mdrubayatalam@gmail.com>
2026-09-04 12:22:21 +0600

    export and Import

---
+++ check.js
@@ -0,0 +1,5 @@
+import add from "./export_import.js";
+import {subs,pi} from "./export_import.js";
+
+console.log(`Sum:${add(10,12)}`);
+console.log(`Substraction:${subs(10,12)}\nPi value: ${pi}`);
---
+++ export_import.js
@@ -0,0 +1,12 @@
+function add(a,b){
+    return a + b;
+}
+
+export function subs(a,b){
+    if(a>b) return a-b;
+    else return b-a;
+}
+
+export default add;
+
+export const pi = 3.1416;
